1c944d535eca90d9422c8250767be3747a6b2d6d
Charlie Barrett <cbarrett24@comcast.net>
2024-11-13 17:19:41 -0500

    no random error

--- test.mjs
+++ test.mjs
@@ -15,13 +15,13 @@ describe('Tests', function () {
             assert.isTrue(false);
         });
 
-        it('This test fails half the time', function () {
-            // Fail in 50% of cases
-            if (Math.random() < 0.5) {
-                throw new Error('An exception occurred');
-            } else {
-                assert.isTrue(true);
-            }
-        });
+        // it('This test fails half the time', function () {
+        //     // Fail in 50% of cases
+        //     if (Math.random() < 0.5) {
+        //         throw new Error('An exception occurred');
+        //     } else {
+        //         assert.isTrue(true);
+        //     }
+        // });
     });
 });
