71a9737ca84ba612a04c758a6487778ef3e1db82
Charlie Barrett <cbarrett24@comcast.net>
2024-11-13 17:10:40 -0500

    changed test names

--- test.mjs
+++ test.mjs
@@ -1,45 +1,27 @@
 import { assert} from 'chai';
 //const { expect } = chai;
 
-describe('files', function () {
-    describe('export', function () {
-        it('should export pdf', function () {
+describe('Tests', function () {
+    describe('Working', function () {
+        it('This test always passes', function () {
             assert.isTrue(true);
         });
 
-        it('should export html', function () {
-            assert.isTrue(true);
-        });
 
-        it('should export yml', function () {
+    });
+
+    describe('failing', function () {
+        it('This test always fails', function () {
             assert.isTrue(false);
         });
 
-        it('should export text', function () {
+        it('This test fails half the time', function () {
             // Fail in 50% of cases
             if (Math.random() < 0.5) {
                 throw new Error('An exception occurred');
             } else {
                 assert.isTrue(true);
             }
-        });
-    });
-
-    describe('import', function () {
-        it('should import pdf', function () {
-            assert.isTrue(true);
-        });
-
-        it('should import html', function () {
-            assert.isTrue(true);
-        });
-
-        it('should import yml', function () {
-            assert.isTrue(true);
-        });
-
-        it('should import text', function () {
-            assert.isTrue(true);
         });
     });
 });
