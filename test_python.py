dd56d23f3cd529ae4f60300669d2550375c3c68b
Charlie Barrett <cbarrett24@comcast.net>
2025-02-04 16:52:43 -0500

    changed from js to python

---
+++ test_python.py
@@ -0,0 +1,20 @@
+import unittest
+import random
+
+
+class Testing(unittest.TestCase):
+    def test_pass(self):
+        self.assertTrue(True)
+    
+    def test_fail(self):
+        self.assertTrue(False)
+        
+    def test_fails_half_the_time(self):
+        if random.random() < 0.5:
+            raise Exception("An exception occurred")
+        else:
+            self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
